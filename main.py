f440eb6d171dc564e2a4c373b50592a2fe8ce4c7
vprivalova <privalovaa.vv@gmail.com>
2023-09-12 10:18:20 +0700

    commit_4

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 from textblob import TextBlob
-from textblob.sentiments import NaiveBayesAnalyzer
 from googletrans import Translator, constants
-# nltk.download('movie_reviews')
-# nltk.download('punkt')
 
-text = input("Введите текст:") #почистить пробелы
+text = input("Введите текст:")
 
 translator = Translator()
 translation = translator.translate(text)
-# print(translation.src)
-print(translation.text)
 
-if translation.src == 'ru':
+
+if translation.src == 'ru' or translation.src == 'uk':
     text = translation.text
 
-# print(text)
 out = TextBlob(text).correct()
-text_one = TextBlob(
-    text,
-    analyzer=NaiveBayesAnalyzer(),
-)
 
 text_blob_object = TextBlob(text)
 text_sentence = text_blob_object.sentences
-# print(text_sentence)
 text_words = text_blob_object.words
-# print(text_words)
+
+ton = text_blob_object.sentiment.polarity
+if (ton >= -1.0) and (ton < 0):
+    analysis_ton = 'Негативный'
+elif (ton > 0) and (ton <= 1.0):
+    analysis_ton = 'Положительный'
+elif ton == 0:
+    analysis_ton = 'Нейтральный'
 
 vowels = ['E', 'Y', 'U', 'I', 'O', 'A']
 text_separ = text_blob_object.words.singularize()
@@ -38,18 +35,6 @@ for i in range(len(text_separ)):
 ASL = len(text_words) / len(text_sentence)
 ASW = v_count / len(text_words)
 K = 206.835 - 1.015 * ASL - 84.6 * ASW
-K_ru = 206,835 - 1,3 * ASL - 60,1 * ASW
-
-
-t0 = TextBlob(text)
-t = t0
-ton = t.polarity
-if (ton >= -1.0) and (ton < 0):
-    analysis_ton = 'Негативный'
-elif (ton > 0) and (ton <= 1.0):
-    analysis_ton = 'Положительный'
-elif ton == 0:
-    analysis_ton = 'Нейтральный'
 
 
 print('Предложений:', len(text_sentence))
