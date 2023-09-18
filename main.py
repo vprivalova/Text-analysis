04dae9e05cb776a2ebe06f83cf794c2038560382
vprivalova <privalovaa.vv@gmail.com>
2023-09-18 23:23:49 +0700

    commit_5

--- main.py
+++ main.py
@@ -1,59 +1,64 @@
 from textblob import TextBlob
-from googletrans import Translator, constants
+from googletrans import Translator
+import ru_local as ru
 
-text = input("Введите текст:")
+RU_VOWELS = 'ЕЁУЫАОЭЯИЮёуеоаыяию'
+ENG_VOWELS = 'EYUIOAeyuioa'
+v_count = 0
 
-translator = Translator()
-translation = translator.translate(text)
-
-
-if translation.src == 'ru' or translation.src == 'uk':
-    text = translation.text
-
-out = TextBlob(text).correct()
-
+text = input(ru.TEXT_INPUT)
 text_blob_object = TextBlob(text)
 text_sentence = text_blob_object.sentences
 text_words = text_blob_object.words
 
+for i in range(len(text)):
+    if text[i] in RU_VOWELS or text[i] in ENG_VOWELS:
+        v_count = v_count + 1
+
+asl = len(text_words) / len(text_sentence)
+aws = v_count / len(text_words)
+
+print(ru.SENTENCES, len(text_sentence))
+print(ru.WORDS, len(text_words))
+print(ru.SYLLABLES, v_count)
+print(ru.ASL, asl)
+print(ru.AWS, aws)
+
+
+translator = Translator()
+detection = translator.detect(text)
+
+if detection.lang == 'ru' or detection.lang == 'uk':
+    f_index = 206.835 - 1.3*asl - 60.1*aws
+else:
+    f_index = 206.835 - 1.015*asl - 84.6*aws
+
+print(ru.FLASH_INDEX, f_index)
+
+
+if f_index > 50:
+    if f_index > 80:
+        print(ru.VERY_EASY_TEXT)
+    else:
+        print(ru.EASY_TEXT)
+else:
+    if f_index > 25:
+        print(ru.HARD_TEXT)
+    else:
+        print(ru.VERY_HARD_TEXT)
+
+
+if detection.lang == 'ru' or detection.lang == 'uk':
+    translation = translator.translate(text)
+    text_blob_object = TextBlob(translation.text)
+
 ton = text_blob_object.sentiment.polarity
 if (ton >= -1.0) and (ton < 0):
-    analysis_ton = 'Негативный'
+    analysis_ton = ru.NEGATIVE
 elif (ton > 0) and (ton <= 1.0):
-    analysis_ton = 'Положительный'
+    analysis_ton = ru.POSITIVE
 elif ton == 0:
-    analysis_ton = 'Нейтральный'
+    analysis_ton = ru.NEUTRAL
 
-vowels = ['E', 'Y', 'U', 'I', 'O', 'A']
-text_separ = text_blob_object.words.singularize()
-v_count = 0
-for i in range(len(text_separ)):
-    for j in text_separ[i]:
-        if j in vowels or j.upper() in vowels:
-            v_count += 1
-
-ASL = len(text_words) / len(text_sentence)
-ASW = v_count / len(text_words)
-K = 206.835 - 1.015 * ASL - 84.6 * ASW
-
-
-print('Предложений:', len(text_sentence))
-print('Слов:', len(text_words))
-print('Слогов:', v_count)
-print('Средняя длина предложений в слогах:', ASL)
-print('Средняя длина слов в слогах:', ASW)
-print('Индекс удобочитаемости Флеша:', K)
-
-if K > 50:
-    if K > 80:
-        print('Текст очень легко читается (для младших классов)')
-    else:
-        print('Простой текст (для школьников)')
-else:
-    if K > 25:
-        print('Текст немного трудно читать (для студентов)')
-    else:
-        print('Текст трудно читать (для выпускников ВУЗов)')
-
-print('Тональность текста:', analysis_ton)
-print('Объективность:', round(1 - TextBlob(text).subjectivity, 2) * 100, '%', sep='')
+print(ru.TONALITY, analysis_ton)
+print(ru.OBJECTIVITY, round(1 - text_blob_object.subjectivity, 2) * 100, '%', sep='')
